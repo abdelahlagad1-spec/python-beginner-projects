3d8ef9950c8fcf8b83c5335215a8b62f6e4431e6
abdelaala-coder <abdelahlagad1@gmail.com>
2026-03-25 16:38:02 +0200

    Update notes_file.py

---
+++ Python_Beginner_Projects/notes_file.py
@@ -0,0 +1,4 @@
+with open("notes.txt", "w") as file:
+    file.write("Programming Concepts:\n")
+    file.write("1. Variables\n2. Functions\n3. Classes\n4. Error Handling\n5. Data Structures\n")
+print("notes.txt created")
